6cf6d3cb5ca8e44bb1255488dd534714852b832a
lufe0 <lufe089@gmail.com>
2018-11-20 10:18:10 -0500

    ADD local settings e info del modelo de prueba

---
+++ envcorin/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lufe0\git\ejemplodjango\envcorin\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ estacionServicio/estacionApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EstacionappConfig(AppConfig):
+    name = 'estacionApp'
